677218c4d46f643213f6ab5b8f0704205448f4b2
605682 <robles-alcaraz_jesse@bah.com>
2021-03-23 10:00:48 -0400

    Adding script to update embeddings in elasticsearch.

---
+++ update/app/vectorizetext.py
@@ -0,0 +1,23 @@
+import os
+
+from haystack.document_store.elasticsearch import ElasticsearchDocumentStore
+from haystack.retriever.dense import DensePassageRetriever
+
+document_store = ElasticsearchDocumentStore(
+    host=os.environ.get("ELASTIC_HOST"),
+    username=os.environ.get("ELASTIC_USER"), 
+    password=os.environ.get("ELASTIC_PASSWORD"),
+    index=os.environ.get("QA_INDEX")
+)
+
+retriever = DensePassageRetriever(document_store=document_store,
+                query_embedding_model="facebook/dpr-question_encoder-single-nq-base",
+                passage_embedding_model="facebook/dpr-ctx_encoder-single-nq-base",
+                max_seq_len_query=64,
+                max_seq_len_passage=256,
+                batch_size=16,
+                use_gpu=False,
+                embed_title=False,
+                use_fast_tokenizers=True)
+
+document_store.update_embeddings(retriever)
